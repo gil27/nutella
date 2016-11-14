79424bef542b9e0545549e04481d982b79562fa8
Gil Gomes <gilgomesp@gmail.com>
2016-11-13 22:58:19 -0200

    Fetching issues from rails api

--- frontend/app/app.module.ts
+++ frontend/app/app.module.ts
@@ -1,13 +1,15 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }   from './app.component';
-import { IssuesComponent } from './issues/issues.component';
-import { IssueService } from './issues/issue.service';
+import { NgModule }          from '@angular/core';
+import { BrowserModule }     from '@angular/platform-browser';
+import { HttpModule }        from '@angular/http';
+
+import { AppComponent }      from './app.component';
+import { IssuesComponent }   from './issues/issues.component';
+import { IssueService }      from './issues/issue.service';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, HttpModule ],
   providers:    [ IssueService ],
   declarations: [ AppComponent, IssuesComponent ],
-  bootstrap:    [ AppComponent, IssuesComponent ]
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- frontend/app/issues/issue.ts
+++ frontend/app/issues/issue.ts
@@ -1,17 +1,9 @@
 export class Issue {
+  author_email: string;
+  body: string;
+  created_at: string;
   id: number;
   name: string;
+  status: string;
+  updated_at: string;
 }
-
-export const ISSUES: Issue[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];

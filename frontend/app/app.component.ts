8cbeb6faf28678cdc9b6cbbfdef4d05258871937
Gil Gomes <gilgomesp@gmail.com>
2016-11-15 09:54:05 -0200

    Displaying issues inside new layout

--- frontend/app/app.component.ts
+++ frontend/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <h1>Nutella Issue Tracker!</h1>
   <my-issues></my-issues>`
 })
 export class AppComponent { }

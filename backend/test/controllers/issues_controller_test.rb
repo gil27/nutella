5a40e125f64bb5b66bb6ebec617726c418ab5e21
Gil Gomes <gilgomesp@gmail.com>
2016-11-15 13:22:28 -0200

    align to two spaces ;)

--- backend/test/controllers/issues_controller_test.rb
+++ backend/test/controllers/issues_controller_test.rb
@@ -1,21 +1,22 @@
 require 'test_helper'
 
 class IssuesControllerTest < ActionDispatch::IntegrationTest
-   fixtures :issues
+  fixtures :issues
 
-   def test_create_issue
-	    get "/issues"
-	    assert_equal 200, status
+  def test_create_issue
+    get "/issues"
+    assert_equal 200, status
 
-	    # post the issue
-	    post '/issues', params: { issue: {
-                                        name: issues(:issue1).name,
-                                        body: issues(:issue1).body,
-                                        author_email: issues(:issue1).author_email,
-                                        status: issues(:issue1).status
-                                      }
-                               }
-	    issue = JSON.parse(response.body)
-        assert_equal "issue1", issue['name']
-   end
+    # post the issue
+    post '/issues', params: { issue: {
+        name: issues(:issue1).name,
+        body: issues(:issue1).body,
+        author_email: issues(:issue1).author_email,
+        status: issues(:issue1).status
+      }
+    }
+
+    issue = JSON.parse(response.body)
+    assert_equal "issue1", issue['name']
+  end
 end

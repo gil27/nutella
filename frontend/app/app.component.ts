fdd25a7f9d5f3b5d4403020381d6559a2b4e6ed5
Gil Gomes <gilgomesp@gmail.com>
2016-11-23 00:37:43 -0200

    issues path :smile:

--- frontend/app/app.component.ts
+++ frontend/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <my-issues></my-issues>
-  <router-outlet></router-outlet>
+  <div class="pure-menu pure-menu-horizontal">
+      <a href="#" class="pure-menu-heading nutella-brand">NUTELLA</a>
+      <ul class="pure-menu-list">
+        <li class="pure-menu-item pure-menu-selected">
+          <a routerLink="/issues" class="pure-menu-link">Issues</a>
+        </li>
+        <li class="pure-menu-item"><a href="https://github.com/gil27/nutella/issues" class="pure-menu-link">Contact</a></li>
+      </ul>
+    </div>
+
+    <router-outlet></router-outlet>
   `
 })
 export class AppComponent { }

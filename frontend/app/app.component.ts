5bb62c8971ec91de1f6bbe2694eb3448c86d8c7c
Gil Gomes <gilgomesp@gmail.com>
2016-11-23 00:29:40 -0200

    Make the router available

--- frontend/app/app.component.ts
+++ frontend/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <my-issues></my-issues>`
+  <my-issues></my-issues>
+  <router-outlet></router-outlet>
+  `
 })
 export class AppComponent { }
